fa4a0cfc457414f31da9d07c27fe3fbccc768db8
tangyanbo <tangyanbo1110@126.com>
2015-07-30 17:27:10 +0800

    commit

---
+++ redis/src/test/java/com/bypay/redis/JedisClusterTest.java
@@ -0,0 +1,20 @@
+package com.bypay.redis;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+public class JedisClusterTest {
+
+	
+	public void test(){
+		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+		//Jedis Cluster will attempt to discover cluster nodes automatically
+		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 7379));
+		JedisCluster jc = new JedisCluster(jedisClusterNodes);
+		jc.set("foo", "bar");
+		String value = jc.get("foo");
+	}
+}
